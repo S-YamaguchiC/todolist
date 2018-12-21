a85340323a17a5b1724e93935728d04f224e4f71
S-YamaguchiC <cist.b216.s.yamaguchi@gmail.com>
2018-12-21 15:25:02 +0900

    commit

---
+++ src/main/java/com/mytodo/todolist/login/LoginController.java
@@ -0,0 +1,19 @@
+package com.mytodo.todolist.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String LoginForm(Model model) {
+        model.addAttribute("Title", "TODO管理アプリ（仮）");
+
+        return "login/loginTpl";
+    }
+
+}
