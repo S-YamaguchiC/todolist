f72ecd1bc85d12c18145a49c3bd6e0e4e59a7477
S-YamaguchiC <cist.b216.s.yamaguchi@gmail.com>
2018-12-24 20:27:48 +0900

    ログアウト時にセッションを破棄

--- src/main/java/com/mytodo/todolist/logout/LogoutController.java
+++ src/main/java/com/mytodo/todolist/logout/LogoutController.java
@@ -1,16 +1,32 @@
 package com.mytodo.todolist.logout;
 
+import com.mytodo.todolist.SessionManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import java.util.Objects;
 
 @Controller
 @RequestMapping("logout")
+@SessionAttributes("scopedTarget.sessionManager")
 public class LogoutController {
 
+    @Autowired
+    SessionManager sessionManager;
+
     @RequestMapping(method = RequestMethod.GET)
-    public String Logout() {
+    public String Logout(SessionStatus status) {
         //認証がなければログインページに
-        return "logout/logoutTpl";
+        if (Objects.isNull(sessionManager.getUserid())) {
+            return "login/loginTpl";
+        } else {
+            // Sessionを切る処理
+            status.setComplete();
+            return "logout/logoutTpl";
+        }
     }
 }
